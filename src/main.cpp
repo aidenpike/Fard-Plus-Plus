21fc1d330b67378c3c834941eb402b46aefef81a
Selisine <Aidenpike113@gmail.com>
2022-09-13 09:58:17 -0400

    Add ability to look for .fpp file

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,21 @@
 #include <iostream>
+#include <fstream>
 
 using namespace std;
+using std::ios;
+using std::ifstream;
 
 int main(){
-  cout << "fard" << endl;
+    ifstream fard;
+
+    //EXTREMELY IMPORTANT:
+    cout << "fard" << endl;
+
+    fard.open("fard.fpp",ios::in);
+
+    if (fard.is_open()){
+        cout << ".fpp file found!" << endl;
+    }
+    else
+        cout << ".fpp file not found." << endl;
 }
